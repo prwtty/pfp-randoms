20e05ddb325c3362ff1391b3c5ccc50a9d8bfc47
prwtty <100978414+prwtty@users.noreply.github.com>
2022-04-22 22:42:46 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 class config {
     constructor() {
-        this.token = "TOKEN"
+        this.token = "OTQ3MjAxNzMwNjIwMzAxNDA0.Yhp0Wg.Zu-MbYFg1SEf2AR5rwNR78_IFuQ"
         this.time = 10000
         this.color = "480ca8"
         this.channels = []
